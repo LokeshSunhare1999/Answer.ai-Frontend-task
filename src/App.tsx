93510db3a812c7d63e02afa3bde4748e8020aeaf
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-11-11 00:24:45 +0530

    remove un used code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
